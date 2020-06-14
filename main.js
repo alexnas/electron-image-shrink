26f9bea0c083ef60552931ee9581ee232ad8d8f6
alexnas <alexnas2python@gmail.com>
2020-06-14 17:08:13 +0300

    Initial commit. Add basic Electron app structure.

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+
+// Set environment
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+const isLinux = process.platform === 'linux' ? true : false;
+const isWindows = process.platform === 'darwin' ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: 500,
+    height: 600,
+    icon: './assets/icons/linux/icon.png',
+    resizable: isDev ? true : false,
+  });
+
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile('app/index.html');
+}
+
+app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
+app.allowRendererProcessReuse = true;
